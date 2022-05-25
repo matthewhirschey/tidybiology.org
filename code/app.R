060130f058754faa9b22b8a037c4b59e1f117867
matthewhirschey <matthew@hirschey.org>
2022-05-24 21:10:55 -0400

    proto-app
    working parent-child chunks in proto learnr app

--- code/app.R
+++ code/app.R
@@ -1,13 +1,7 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+#install libraries
+library(tidyverse)
 library(shiny)
+library(learnr)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
